be5e9a61f1c157ae1f10bb219c9854925bb9ffa0
Josh Parkker <jparker@sherpas.ai>
2024-08-28 18:55:44 -0600

    add the call to the clock tick with the 60 fps frame rate

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill("#000000")
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # This method should be called once per frame.
+        # It will compute how many milliseconds have passed since the previous call.
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()

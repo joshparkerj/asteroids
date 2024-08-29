2d2e782da7f86f0eaa7232c9baf142cff289c952
Josh Parkker <jparker@sherpas.ai>
2024-08-28 18:47:19 -0600

    start flipping the pygame display

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill("#000000")
-
-
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
